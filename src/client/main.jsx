1b7969feeb56e3c4cad2b212ce471f4a8b0f2080
paa123en <08.astm@gmail.com>
2020-01-13 22:31:46 +0900

    create minimal react import network reactなどを外部から取得し使用できるまで。

---
+++ src/client/main.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div>
+	  <span>Hello React comming !!</span>
+    </div>
+  );
+}
+
+const target = document.querySelector("#app");
+ReactDOM.render(<App />, target);
+
